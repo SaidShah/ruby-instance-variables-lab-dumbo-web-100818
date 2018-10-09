67af68b2fae47142cd910bd327ee15d102260b92
said shah <saidmysr20@gmail.com>
2018-10-09 20:15:20 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,11 @@
 class Dog
 
-attr_accessor :this_dogs_name
+def name=(dog_name)
+  @this_dogs_name = dog_name
+end
 
-def initialize(dog_name)
- @this_dogs_name = dog_name
+def name
+  @this_dogs_name
 end
 
 end
